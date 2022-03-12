1b7de8d143428415a64e525ae2d365561079df60
Aiden Isaac <aidenisaac08@gmail.com>
2022-03-12 19:13:40 +0800

    notes

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ fn what_is_your_name() -> String {
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("{:?}", name);
+    if name == "bert" || name == "steve" {
+        println!("Welcome {}", name);
+    } else {
+        println!("Sorry, you aren't on the list, {}", name)
+    }
 }
 
-// Line 13 adds a debugging parameter which helps show us the exact value of the variable
-// The output comes out as "test\n", and \n is the ENTER indent. This is called a carriage return.
-// Rust includes a trim function to remove these characters. Which is a good idea since you need the variables to match with what you have on mind ("Name" =/= "Name\n")
-// It's also a good idea to always make the output lowercase so then the letters will always match ("nAmE" =/= "name")
+// Line 13 shows boolean logic, || means "or", && means "and", ! means not
