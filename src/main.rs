8c02db0d4646d62a4259cc76fabe5b35b2985b18
Aiden Isaac <aidenisaac08@gmail.com>
2022-03-13 17:44:01 +0800

    notes on array

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,36 @@ fn what_is_your_name() -> String {
         .expect("Failed to read line");
     your_name.trim().to_lowercase()
 }
+
 fn main() {
+    let visitor_list = ["bert", "steve", "fred"];
+
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("{:?}", name);
+
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list.");
+    }
+
 }
 
-// Line 13 adds a debugging parameter which helps show us the exact value of the variable
-// The output comes out as "test\n", and \n is the ENTER indent. This is called a carriage return.
-// Rust includes a trim function to remove these characters. Which is a good idea since you need the variables to match with what you have on mind ("Name" =/= "Name\n")
-// It's also a good idea to always make the output lowercase so then the letters will always match ("nAmE" =/= "name")
+// Rust's simplest list type is an array
+// There are two rules to the values in an array:
+//     1. The values must be of the same type
+//     2. The array cannot change in size
+// Line 12 shows the declaration of an array with string literals (&str types)
+// If you wanted to declare the array type, the syntax would be:
+//     let visitor_list : [&str;3] = ...
+// Index numbering is similar in Python, it counts from 0
+// Rust has two types of strings,
+//     str; string literals entered in source code, generally unchanging
+//     String; dynamic because you can store location, length and capacity
