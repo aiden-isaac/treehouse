ad90bf58201e9cfd10a050b1fed9583e6e27c6bd
Aiden Isaac <aidenisaac08@gmail.com>
2022-03-17 15:51:37 +0800

    notes on option type

--- src/main.rs
+++ src/main.rs
@@ -33,14 +33,21 @@ fn main() {
         Visitor::new("fred", "Wow, who invited Fred?"),
     ];
 
-    println!("Hello, what's your name?");        
+    println!("Hello, what's your name?");
     let name = what_is_your_name();
-        
-    let known_visitor = visitor_list
-        .iter()        
-        .find(|visitor| visitor.name == name);
+
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
     match known_visitor {
         Some(visitor) => visitor.greet_visitor(),
         None => println!("You are not on the visitor list. Please leave."),
     }
 }
+
+// The find closure returns an Option type.
+// Options either contain a value or they don’t.
+// Rust options are an enumeration.
+// Options have two possible values: Some(x) and None.
+// There are many ways to interact with and extract data from an option.
+// In this program, line 42 uses match with the option data.
+// match some(visitor) checks to see if the option has data and makes the contents of the option available to the code.
+// The fat arrow in line 43 and 44 (=>) denotes the code to execute for this match.
